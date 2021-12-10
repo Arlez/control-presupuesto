67ab1576ba3b227fce10c83aa0ca0448676c1a85
arlez <rainmake123@gmail.com>
2021-12-10 14:52:33 -0300

    archivos iniciales

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react/cjs/react.production.min"
+import Header from "./components/Header"
+
+function App() {
+ 
+
+  return (
+    <>
+      <Header/>
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,25 @@
+const NuevoPresupuesto = () => {
+    return (
+        <div className="contenedor-presupuesto contenedor sombra">
+            <form className="formulario">
+                <div className="campo">
+                    <label htmlFor="">Definir Presupuesto</label>
+
+                    <input 
+                        type="text" 
+                        className="nuevo-presupuesto"
+                        placeholder="Añade tu Presupuesto"
+                    />
+                </div>
+
+                <input 
+                    type="submit" 
+                    value="Añadir"
+                />
+
+            </form>
+        </div>
+      );
+}
+ 
+export default NuevoPresupuesto;
